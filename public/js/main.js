5608c9111ae9d6670b0f53ca07c5dde474cb6e77
Akrit Shrestha <akrit.act360@gmail.com>
2020-08-02 14:37:13 +0545

    mario image can move

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,38 @@
+import Compositor from "./Compositor.js";
 import { loadLevel } from "./loaders.js";
 import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
+import { createBackgroundLayer } from "./layers.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-    for (let x = x1; x < x2; ++x) {
-      for (let y = y1; y < y2; ++y) {
-        sprites.drawTile(background.tile, context, x, y);
-      }
-    }
-  });
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(context) {
+    sprite.draw("idle", context, pos.x, pos.y);
+  };
 }
 
 Promise.all([
   loadMarioSprite(),
   loadBackgroundSprites(),
   loadLevel("1-1"),
-]).then(([marioSprite, sprites, level]) => {
-  level.backgrounds.forEach((bg) => {
-    drawBackground(bg, context, sprites);
-  });
-  marioSprite.draw("idle", context, 64, 64);
+]).then(([marioSprite, backgroundSprites, level]) => {
+  const comp = new Compositor();
+  comp.layers.push(createBackgroundLayer(level.backgrounds, backgroundSprites));
+
+  const pos = {
+    x: 64,
+    y: 64,
+  };
+
+  comp.layers.push(createSpriteLayer(marioSprite, pos));
+
+  function update() {
+    comp.draw(context);
+    pos.x += 2;
+    pos.y += 1;
+    requestAnimationFrame(update);
+  }
+
+  update();
 });
--- public/js/sprites.js
+++ public/js/sprites.js
@@ -1,11 +1,11 @@
-import { loadImage } from "./loaders.js";
 import SpriteSheet from "./SpriteSheet.js";
+import { loadImage } from "./loaders.js";
 
 export function loadMarioSprite() {
   return loadImage("/img/characters.gif").then((image) => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define("idle", 276, 44, 16, 16);
-    return sprites;
+    const mario = new SpriteSheet(image, 16, 16);
+    mario.define("idle", 276, 44, 16, 16);
+    return mario;
   });
 }
 
